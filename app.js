5a58f6d85a50cd669c6177b3e8d77c3c4cd4f06d
Ngozi-ka <nwekengozika5@gmail.com>
2025-02-28 19:37:57 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+const amount = document.querySelector("#amount");
+const term = document.querySelector("#term");
+const rate = document.querySelector("#rate");
+const inactive = document.querySelector(".inactive");
+const active = document.querySelector(".active");
+const monthlyAmount = document.querySelector(".monthly-amount");
+const totalAmount = document.querySelector(".total-amount");
+const repayment = document.querySelector("#repayment");
+const interest = document.querySelector("#interest");
+const clearAll = document.querySelector("#clearAll");
+const mortgageForm = document.getElementsByTagName("mortgageform");
+
+
+mortgageForm.addEventListener("submit", function(e){
+   alert("hello")
+
+})
+
+})
